de1913c53d3ee866b02a82e5c3dba0fb8d1923c6
gagankumarsaini <gagan.k@w3dev.email>
2024-06-10 16:51:21 +0530

    "Added new user controller functions for changing password, getting current user, updating account details, avatar, and cover image, and fetching user channel profile and watch history."

--- src/models/subscriptions.model.js
+++ src/models/subscriptions.model.js
@@ -1,1 +1,19 @@
 import mongoose,{Schema} from "mongoose";
+
+ const subscriptioonSchema = new Schema(
+    {
+        subscriber:{
+            type:Schema.Types.ObjectId, //one who is subscribing
+            ref:"User"
+        },
+        channel:{
+            type:Schema.Types.ObjectId, //one who is subscriber is subscribing
+            ref:"User"
+        }
+    },
+    {
+        timestamps: true
+        }
+ )
+
+ export const Subscription = mongoose.model("Subsciption",subscriptioonSchema)
