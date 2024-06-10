0481e117179ae38a69dcb42671d5781276972f93
gagankumarsaini <gagan.k@w3dev.email>
2024-06-10 23:21:52 +0530

    bug fix in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import uploadOnCloudinary from "../utils/Cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
@@ -136,8 +137,8 @@ res.status(200).cookie("refreshToken",refreshToken,options).cookie("accessToken"
 const logoutUser = asyncHandler(async (req,res)=>{
 
     await User.findByIdAndUpdate(req.user._id,{
-        $set:{
-            refreshToken: undefined
+        $unset:{
+            refreshToken: 1
         }
     },{
         new:true
@@ -150,7 +151,7 @@ const logoutUser = asyncHandler(async (req,res)=>{
     return res.status(200).clearCookie("accessToken",options)
     .clearCookie("refreshToken",options)
     .json(
-        new ApiResponse(200,null,"User logged out successfully")
+        new ApiResponse(200,"User logged out successfully")
     )
 
 });
@@ -201,7 +202,202 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
 }) 
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+    const {currentPassword, newPassword} = req.body
+    const user = await User.findById(req.user._id)
+    const isPasswordCorrect=await user.isPasswordCorrect(currentPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(401,"Invalid current password")}
+        user.password = newPassword
+        user.save({validateBeforeSave:false})
+        res.status(200).json(new ApiResponse(200,"Password changed successfully"))
+})
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res.status(200).json(new ApiResponse(200,
+        req.user,"current user fetched successfully")
+    )
+})
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName,email} = req.body
+    if(!fullName ||!email){
+        throw new ApiError(400,"Please provide username and email")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id,{
+        fullName,
+        email
+        },{
+            $set:{
+                fullName,
+                email
+            }
+        },{new:true,runValidators:true}).select("-password")
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+const updateAvatar = asyncHandler(async(req,res)=>{
+   const avatarLocalPath = req.file?.path
+   if(!avatarLocalPath){
+    throw new ApiError(400,"Please upload an  avatar image")
+    }
+    const avatar= await uploadOnCloudinary(avatarLocalPath)
+    if(!avatar.url){
+        throw new ApiError(500,"Failed to upload avatar")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id,{
+       $set:{
+        avatar:avatar.url
+        }
+        },{new:true,runValidators:true}).select("-password")
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200,user,"Avatar updated successfully"))
+
+})
+const updateCoverImage = asyncHandler(async(req,res)=>{
+    const coverImageLocalPath = req.file?.path
+    if(!coverImageLocalPath){
+     throw new ApiError(400,"Please upload an  cover Image")
+     }
+     const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+     if(!coverImage.url){
+         throw new ApiError(500,"Failed to upload coverimage")
+     }
+     const user = await User.findByIdAndUpdate(req.user?._id,{
+        $set:{
+            coverImage:coverImage.url
+         }
+         },{new:true,runValidators:true}).select("-password")
+
+         return res
+        .status(200)
+        .json(new ApiResponse(200,user,"cover image updated successfully"))
+ 
+})
+ const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing");
+    }
+    
+    try {
+        const channel = await User.aggregate([
+            {
+                $match: {
+                    username: username.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedTo"
+                }
+            },
+            {
+                $addFields: {
+                    subscribersCount: { $size: "$subscribers" },
+                    subscribedToCount: { $size: "$subscribedTo" },
+                    isSubscribed: {
+                        $cond: {
+                            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    fullName: 1,
+                    username: 1,
+                    subscribedToCount: 1,
+                    subscribersCount: 1,
+                    avatar: 1,
+                    coverImage: 1,
+                    isSubscribed: 1,
+                    email: 1
+                }
+            }
+        ]);
+            
+        if (!channel.length) {
+            throw new ApiError(404, "Channel does not exist");
+        }
+    
+        res.status(200).json(new ApiResponse(200, channel,"User channel fetched successfully"));
+    } catch (error) {
+        throw new ApiError(401, error?.message || "error")
+    }
+    
+})
+const getWatchHistory = asyncHandler(async (req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+        }
+    },{
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[{
+                            $project:{
+                                _id:1,
+                                username:1,
+                                avatar:1,
+                                fullName:1
+                            }
+                            
+                        }]
+                    }
+                }
+            ]
+        }
+    },{
+        $addFields:{
+            owner:{
+                $first:"$owner"
+            }
+        }
+    }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200,user[0].watchHistory,"User watch history fetched Successfully")
+    )
+})
 
 
-
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+export{registerUser
+    ,loginUser
+    ,logoutUser
+    ,refreshAccessToken
+    ,changeCurrentPassword
+    ,getCurrentUser
+    ,updateAccountDetails
+    ,updateAvatar
+    ,updateCoverImage
+    ,getUserChannelProfile
+    ,getWatchHistory}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccessToken} from "../controllers/user.controller.js";
+import { 
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verfiyJwt } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -17,8 +29,16 @@ router.route("/register").post(
     registerUser
 )
 router.route("/login").post(loginUser)
-
 router.route("/logout").post(verfiyJwt,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verfiyJwt,changeCurrentPassword)
+router.route("/current-user").get(verfiyJwt,getCurrentUser)
+router.route("/update-account").patch(verfiyJwt,updateAccountDetails)
+router.route("/avatar").patch(verfiyJwt,upload.single("avatar"),updateAvatar)
+router.route("/cover-image").patch(verfiyJwt,upload.single("coverImage"),updateCoverImage)
+router.route("/c/:username").get(verfiyJwt,getUserChannelProfile)
+router.route("/history").get(verfiyJwt,getWatchHistory)
+
+
 
 export default router
